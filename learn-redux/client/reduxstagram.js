b06a0848bfc490fd705ad968e48c762442668cf3
leer <ladesigns90@gmail.com>
2020-08-25 16:34:01 -0400

    Created components directory along with the three component js files. Created Main, PhotoGrid, and Single component files.

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,12 @@
 // let's go!
+import React from 'react';
+
+import { render } from 'react-dom';
+
+// Import CSS
+import css from './styles/style.styl';
+
+// mport Components
+import Main from './components/Main';
+
+render(<Main />, document.getElementById('root'));
